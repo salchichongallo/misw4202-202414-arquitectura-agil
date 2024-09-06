a56d05638f5960b71104c92d9ab212e445775592
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-09-05 23:23:06 -0500

    feat(balancer): initial forward requests

--- balancer.py
+++ balancer.py
@@ -1,11 +1,18 @@
 import os
-from flask import Flask
+from requests import post
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return {'message': 'Hello, Balancer!'}
+
+@app.post('/forward-call')
+def forward_call():
+    # TODO: Forward call to the next available microservice
+    response = post('http://localhost:5002/call', json=request.json)
+    return { 'data': response.json(), 'forwarded': True }
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.environ.get('PORT', 5001))

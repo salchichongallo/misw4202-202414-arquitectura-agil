d06b7318148d79e32b5c7dab83fb5a6b1e60f34d
leinerbarrios <lj.barrios@uniandes.edu.co>
2024-09-07 21:23:01 -0500

    feat: 🎸 Comunicación con el microservicio de monitor

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -1,6 +1,7 @@
 from api_gateway import create_app
 from flask_restful import Resource, Api
 from flask import Flask, request
+import requests
 
 app = create_app('default')
 app_context = app.app_context()
@@ -10,6 +11,10 @@ api = Api(app)
 
 class VistaLlamadas(Resource):
     def post(self):
-        return { 'data': request.json }
+        monitor_ms = "http://127.0.0.1:5001/assign-call"
+        content = requests.post(monitor_ms, json=request.json)
+        response = content.json()
+        return response
+
 
 api.add_resource(VistaLlamadas, '/call')

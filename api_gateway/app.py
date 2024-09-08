bbb1324dd09aebcc85fe352e25499506a8d4ac62
leinerbarrios <lj.barrios@uniandes.edu.co>
2024-09-08 11:50:23 -0500

    feat: 🎸 Se crea lógica para almacenar las respuestas en TXT

--- api_gateway/app.py
+++ api_gateway/app.py
@@ -1,7 +1,10 @@
 from api_gateway import create_app
 from flask_restful import Resource, Api
-from flask import Flask, request
+from flask import request
 import requests
+import time
+from datetime import datetime
+import os
 
 app = create_app('default')
 app_context = app.app_context()
@@ -9,11 +12,37 @@ app_context.push()
 
 api = Api(app)
 
+file_name = "calls_response.txt"
+file_path = os.path.join("responses", file_name)
+counter = 0
+
+def create_file():
+    try:
+        os.remove("responses/calls_response.txt")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    except OSError:
+        pass
+
 class VistaLlamadas(Resource):
+    create_file()
+
     def post(self):
+        global counter
+        counter += 1
+        start_time = time.time()
         monitor_ms = "http://127.0.0.1:5001/assign-call"
-        content = requests.post(monitor_ms, json=request.json)
-        response = content.json()
+        requests.post(monitor_ms, json=request.json)
+        end_time = time.time()
+        response_time = round((end_time - start_time) * 1000, 2)
+        response = {
+            "call_number": counter,
+            "call_date": datetime.now().strftime("%d/%m/%Y, %H:%M:%S"),
+            "response_time": f"{response_time} ms"
+        }
+
+        with open(file_path, "a") as file:
+            file.write(str(response) + "\n")
+
         return response
 
 

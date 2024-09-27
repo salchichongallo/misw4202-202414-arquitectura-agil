194967898f4d4465e0287b32acadcc18732af4ca
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-09-27 06:21:46 -0500

    feat(report): get report by id

--- experimento-seguridad/microservice-reportes/report_controller.py
+++ experimento-seguridad/microservice-reportes/report_controller.py
@@ -1,7 +1,15 @@
-from flask import jsonify
+from flask import jsonify, make_response
 from flask_restful import Resource
+from report_models import db, ReportModel
 
 
 class ReportController(Resource):
     def get(self, report_id):
-        return jsonify({ 'report_id': report_id })
+        # TODO: check if user is authorized to view this report
+        return self.get_report(report_id)
+
+    def get_report(self, report_id):
+        report = db.session.query(ReportModel).get(report_id)
+        if report:
+            return jsonify(report.to_dict())
+        return make_response(jsonify({ 'message': 'Report not found' }), 404)

f4945e7cf129d803f82e6d39b7be410c989df162
Jimmy Murillo <salchichongallo1997@gmail.com>
2024-09-27 18:53:05 -0500

    feat(report): pass request throw auth ms

--- experimento-seguridad/microservice-reportes/report_controller.py
+++ experimento-seguridad/microservice-reportes/report_controller.py
@@ -1,12 +1,23 @@
-from flask import jsonify, make_response
+import requests
 from flask_restful import Resource
 from report_models import db, ReportModel
+from flask import jsonify, make_response, request
+
+
+def get_role_url(permission):
+    return f'http://localhost:5001/validate-role?permiso={permission}'
 
 
 class ReportController(Resource):
     def get(self, report_id):
-        # TODO: check if user is authorized to view this report
+        response = self.check_read_permission()
+        if not response.ok:
+            return make_response(response.json(), response.status_code)
         return self.get_report(report_id)
+
+    def check_read_permission(self):
+        url = get_role_url(permission='PUEDE_VER_REPORTES')
+        return requests.get(url, headers=request.headers)
 
     def get_report(self, report_id):
         report = db.session.query(ReportModel).get(report_id)
